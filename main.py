84d1415fa7d610c4f71e27a12765a1b5cd52e443
Garyyys <Allgarryyys@gmail.com>
2021-04-21 00:16:11 +0300

    Rick and Morty parser

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import json
+from r_and_m import get_characters, Character
+
+if __name__ == '__main__':
+    FILE_NAME = input()
+
+    if os.path.exists(FILE_NAME):
+        f = open(FILE_NAME, 'r')
+        characters = json.loads(f.read())
+        f.close()
+        print('loaded from disk')
+    else:
+        characters = get_characters()
+        f = open(FILE_NAME, 'w')
+        f.write(json.dumps(characters))
+        f.close()
+        print('loaded from api and saved to disk')
+
+    characters = [Character(c['name'], c['species']) for c in characters]
